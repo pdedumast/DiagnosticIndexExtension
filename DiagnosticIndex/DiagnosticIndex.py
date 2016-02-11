d5ac1d845dfc8bd0b4851279c62530f850d85011
Laura PASCAL <laurajulia.pascal@gmail.com>
2016-02-11 17:36:11 -0500

    ENH: Adding the User's Interface and its implementation in .py file

---
+++ DiagnosticIndex/DiagnosticIndex.py
@@ -0,0 +1,100 @@
+import os, sys
+import csv
+import unittest
+from __main__ import vtk, qt, ctk, slicer
+from slicer.ScriptedLoadableModule import *
+from types import *
+
+
+class DiagnosticIndex(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+  https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
+  """
+
+    def __init__(self, parent):
+        ScriptedLoadableModule.__init__(self, parent)
+        parent.title = "DiagnosticIndex"
+        parent.categories = ["Quantification"]
+        parent.dependencies = []
+        parent.contributors = ["Laura PASCAL (UofM)"]
+        parent.helpText = """
+            """
+        parent.acknowledgementText = """
+            This work was supported by the National
+            Institutes of Dental and Craniofacial Research
+            and Biomedical Imaging and Bioengineering of
+            the National Institutes of Health under Award
+            Number R01DE024450.
+            """
+
+
+class DiagnosticIndexWidget(ScriptedLoadableModuleWidget):
+    def setup(self):
+        ScriptedLoadableModuleWidget.setup(self)
+
+        # widget setup
+        self.logic = DiagnosticIndexLogic(self)
+
+        # --- interface ---
+        loader = qt.QUiLoader()
+        moduleName = 'DiagnosticIndex'
+        scriptedModulesPath = eval('slicer.modules.%s.path' % moduleName.lower())
+        scriptedModulesPath = os.path.dirname(scriptedModulesPath)
+        path = os.path.join(scriptedModulesPath, 'Resources', 'UI', '%s.ui' % moduleName)
+        qfile = qt.QFile(path)
+        qfile.open(qt.QFile.ReadOnly)
+
+        widget = loader.load(qfile, self.parent)
+        self.layout = self.parent.layout()
+        self.widget = widget
+        self.layout.addWidget(widget)
+
+        # Global Variable of the Interface:
+        #TODO
+
+        # ------------------------------------------------------------------------------------
+        #                                   CONNECTIONS
+        # ------------------------------------------------------------------------------------
+        slicer.mrmlScene.AddObserver(slicer.mrmlScene.EndCloseEvent, self.onCloseScene)
+
+    # function called each time that the user "enter" in Longitudinal Quantification interface
+    def enter(self):
+        #TODO
+        pass
+
+    # function called each time that the user "exit" in Longitudinal Quantification interface
+    def exit(self):
+        #TODO
+        pass
+
+    # function called each time that the scene is closed (if Longitudinal Quantification has been initialized)
+    def onCloseScene(self, obj, event):
+        #TODO
+        pass
+
+# ------------------------------------------------------------------------------------
+#                                   ALGORITHM
+# ------------------------------------------------------------------------------------
+
+
+class DiagnosticIndexLogic(ScriptedLoadableModuleLogic):
+    def __init__(self, interface):
+        self.interface = interface
+
+    # === Convenience python widget methods === #
+    def get(self, objectName):
+        return self.findWidget(self.interface.widget, objectName)
+
+    def findWidget(self, widget, objectName):
+        if widget.objectName == objectName:
+            return widget
+        else:
+            for w in widget.children():
+                resulting_widget = self.findWidget(w, objectName)
+                if resulting_widget:
+                    return resulting_widget
+            return None
+
+
+class DiagnosticIndexTest(ScriptedLoadableModuleTest):
+    pass
