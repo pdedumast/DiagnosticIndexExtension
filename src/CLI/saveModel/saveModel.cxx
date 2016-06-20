a933945399e771ad1cb4447212807182c325b724
Laura PASCAL <laurajulia.pascal@gmail.com>
2016-06-20 11:29:07 -0400

    ENH: Improvement of the CLI saveModel
    - Compute the mean between all the meshes
    - The reference of the shape model is now the mean previously computed

--- src/CLI/saveModel/saveModel.cxx
+++ src/CLI/saveModel/saveModel.cxx
@@ -8,6 +8,7 @@
 #include "vtkPolyDataReader.h"
 #include "saveModelCLP.h"
 #include <fstream>
+#include "vtkCellArray.h"
 
 using namespace statismo;
 typedef vtkStandardMeshRepresenter RepresenterType;
@@ -27,14 +28,64 @@ vtkSmartPointer<vtkPolyData> loadVTKPolyData(const std::string& filename) {
 int main(int argc, char ** argv)
 {
     PARSE_ARGS;
-    
+
     if(argc < 7)
     {
         std::cout << "Usage " << argv[0] << " [--groupnumber <int>] [--vtkfilelist <std::vector<std::string>>] [--resultdir <std::string>]" << std::endl;
         return 1;
     }
-    
-    vtkSmartPointer<vtkPolyData> reference = loadVTKPolyData(vtkfilelist[0]);
+
+    // Average of all the VTK meshes
+    //   Mean of the 3 coordinates
+    vtkSmartPointer<vtkPolyData> polydata0 = loadVTKPolyData(vtkfilelist[0]);
+    int numPts = loadVTKPolyData(vtkfilelist[0])->GetPoints()->GetNumberOfPoints();
+    vtkSmartPointer<vtkCellArray> verts = polydata0->GetVerts();
+    vtkSmartPointer<vtkCellArray> lines = polydata0->GetLines();
+    vtkSmartPointer<vtkCellArray> polys = polydata0->GetPolys();
+    vtkSmartPointer<vtkCellArray> strips = polydata0->GetStrips();
+
+    vtkSmartPointer<vtkPolyData> polydata_MeanGroup = vtkSmartPointer<vtkPolyData>::New();
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    points = polydata0->GetPoints();
+    for(int meshID = 1; meshID < vtkfilelist.size(); meshID++)
+    {
+        vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
+        polydata = loadVTKPolyData(vtkfilelist[meshID]);
+        for(int ptID = 0; ptID < numPts; ptID++)
+        {
+            double coord[3];
+            polydata->GetPoint(ptID, coord);
+            double sum[3];
+            points->GetPoint(ptID, sum);
+            for(int dim = 0; dim < 3; dim++)
+            {
+                sum[dim] = sum[dim] + coord[dim];
+            }
+            points->InsertPoint(ptID, sum);
+        }
+    }
+    double mean[3];
+    for(int ptID = 0; ptID < numPts; ptID++)
+    {
+        for(int dim = 0; dim < 3; dim++)
+        {
+            double sum[3];
+            points->GetPoint(ptID, sum);
+            mean[dim] = sum[dim]/vtkfilelist.size();
+        }
+        points->InsertPoint(ptID, mean);
+    }
+
+    //   Creation of the polydata of the mean of the given group
+    polydata_MeanGroup->SetPoints(points);
+    polydata_MeanGroup->SetVerts(verts);
+    polydata_MeanGroup->SetLines(lines);
+    polydata_MeanGroup->SetPolys(polys);
+    polydata_MeanGroup->SetStrips(strips);
+
+    // Creation of the shape model
+    vtkSmartPointer<vtkPolyData> reference = polydata_MeanGroup;
+
     boost::scoped_ptr<RepresenterType> representer(RepresenterType::Create(reference));
     boost::scoped_ptr<DataManagerType> dataManager(DataManagerType::Create(representer.get()));
     for(int j = 0; j < vtkfilelist.size(); j++)
