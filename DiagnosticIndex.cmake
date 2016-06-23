0b64b8c6566a36a1f9c23cca1b33964afa725639
juanprietob <juanprietob@gmail.com>
2016-06-23 17:45:46 -0400

    BUG: Build extension using improved Superbuild mechanism
    The extension build statismo with the static variable

---
+++ DiagnosticIndex.cmake
@@ -0,0 +1,8 @@
+cmake_minimum_required(VERSION 2.8.9)
+
+project(DiagnosticIndex)
+set(LOCAL_PROJECT_NAME DiagnosticIndex)
+
+
+#-----------------------------------------------------------------------------
+add_subdirectory(src)
