978f589de77d4c96330d7c622ada1377b1552a3b
Priscille De Dumast <pdedumast@gmail.com>
2017-03-17 15:48:13 -0400

    ENH: Remove files for 3DSlicer extension

--- DiagnosticIndex.cmake
+++ DiagnosticIndex.cmake
@@ -1,7 +1,7 @@
-cmake_minimum_required(VERSION 2.8.9)
+# cmake_minimum_required(VERSION 2.8.9)
 
-project(DiagnosticIndex)
-set(LOCAL_PROJECT_NAME DiagnosticIndex)
+# project(DiagnosticIndex)
+# set(LOCAL_PROJECT_NAME DiagnosticIndex)
 
 
 #-----------------------------------------------------------------------------
--- README.md
+++
@@ -1,9 +0,0 @@
-# DiagnosticIndex
-
-## Description
-
-DiagnosticIndex is used to define the OA type of a patient according a Classification Groups that you can create.
-
-##License
-
-See License.txt for information on using and contributing.
